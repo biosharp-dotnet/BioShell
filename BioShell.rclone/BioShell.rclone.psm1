7f38f361d3015da597e58fc3d8f0d059fb9ba9d8
Abhinav Sharma <abhi18av@gmail.com>
2020-10-02 23:41:24 +0530

    initial commmit with the raw BioShell.rclone

---
+++ BioShell.rclone/BioShell.rclone.psm1
@@ -0,0 +1,19 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.172
+	 Created on:   	10/2/2020 11:34 PM
+	 Created by:   	abhi1
+	 Organization: 	BioSharp.net
+	 Filename:     	BioShell.rclone.psm1
+	-------------------------------------------------------------------------
+	 Module Name: BioShell.rclone
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
